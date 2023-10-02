7e4519f9f108c9015c52ed8798f559ba5c0765f8
daisuke.abe <commityneo@gmail.com>
2023-10-03 07:57:07 +0900

    メールとパスワードを入力するテキストエリアを追加

--- linne/Login/LoginView.swift
+++ linne/Login/LoginView.swift
@@ -8,8 +8,28 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    // メールアドレス
+    @State private var email: String = ""
+    // パスワード
+    @State private var password: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            // 全体の背景カラー
+            // Color.yellow.ignoresSafeArea()
+            
+            VStack(alignment: .center) {
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                TextField("email address", text: $email)
+                SecureField("password", text: $password)
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    // 後ほど処理を追加
+                }
+            }.padding()
+            
+        }
     }
 }
 
