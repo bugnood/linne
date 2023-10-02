9b5d369f6c8e6253126d7dcdcca71672140dbaf3
daisuke.abe <commityneo@gmail.com>
2023-10-03 07:43:56 +0900

    ログイン画面を追加

---
+++ linne/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  linne
+//
+//  Created by 阿部大輔 on 2023/10/03.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
