02c09b9a3f3aa8d1c035bafaa73e80c700e41d0d
daisuke.abe <commityneo@gmail.com>
2023-10-03 07:39:06 +0900

    Initial Commit

---
+++ linne/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  linne
+//
+//  Created by 阿部大輔 on 2023/10/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ linne/linneApp.swift
@@ -0,0 +1,17 @@
+//
+//  linneApp.swift
+//  linne
+//
+//  Created by 阿部大輔 on 2023/10/03.
+//
+
+import SwiftUI
+
+@main
+struct linneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
