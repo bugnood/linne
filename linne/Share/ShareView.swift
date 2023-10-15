acc5402991642a3b1c06e6090e419f5db1d05b31
daisuke.abe <commityneo@gmail.com>
2023-10-15 22:08:08 +0900

    投稿画面の詳細を追加

--- linne/Share/ShareView.swift
+++ linne/Share/ShareView.swift
@@ -33,13 +33,52 @@ struct ShareView: View {
                                         .overlay(
                                             RoundedRectangle(cornerRadius: 30)
                                                 .stroke(Color.gray, lineWidth: 1))
-                                        .frame(width: 40, height: 40)
+                                        .frame(width: 30, height: 30)
                                         .cornerRadius(30)
                                     Text("test.user")
                                     Spacer()
                                 }
                                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
                                 Text("サンプル")
+                                Divider()
+                                Text("タイトル")
+                                    .fontWeight(.bold)
+                                
+                                HStack {
+                                    VStack(alignment: .leading) {
+                                        HStack {
+                                            Image(systemName: "clock.arrow.circlepath")
+                                                .foregroundColor(.gray)
+                                            Text("2023年10月23日")
+                                                .font(.caption)
+                                        }
+                                        HStack {
+                                            Image(systemName: "clock.arrow.circlepath")
+                                                .foregroundColor(.gray)
+                                            Text("17:00~18:00")
+                                                .font(.caption)
+                                        }
+                                        HStack {
+                                            Image(systemName: "clock.arrow.circlepath")
+                                                .foregroundColor(.gray)
+                                            Text("20人")
+                                                .font(.caption)
+                                        }
+                                        HStack {
+                                            Image(systemName: "clock.arrow.circlepath")
+                                                .foregroundColor(.gray)
+                                            Text("2,000円")
+                                                .font(.caption)
+                                        }
+                                    }
+                                    Spacer()
+                                    Image("userIcon")
+                                        .resizable()
+                                        .frame(width: 180.0, height: 100.0)
+                                        .overlay(
+                                            RoundedRectangle(cornerRadius: 5)
+                                                .stroke(Color.gray, lineWidth: 1))
+                                }
                             }
                             .padding(.all, 10.0)
                             .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
