ffe3036a667a97fafffa767fc26aabd846269bac
daisuke.abe <commityneo@gmail.com>
2023-11-25 21:37:07 +0900

    画面遷移を実装

--- linne/Login/LoginView.swift
+++ linne/Login/LoginView.swift
@@ -15,53 +15,65 @@ struct LoginView: View {
     @State private var password: String = ""
     // 背景色の設定
     let backGroundColor = LinearGradient(gradient: Gradient(colors: [Color.backgroundTop, Color.backgroundBottom]), startPoint: .top, endPoint: .bottom)
+    // ログイン判定フラグ
+    @State private var loginFlag: Bool = false
     
     var body: some View {
         
-        ZStack {
-            // 全体の背景カラー
-            backGroundColor.ignoresSafeArea()
-            
-            VStack(alignment: .leading) {
-                // タイトル
-                Text("Linneにログイン")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .padding(.vertical, 60.0)
-                // メールアドレス
-                Text("メールアドレス")
-                    .foregroundColor(.white)
-                TextField("", text: $email)
-                    .autocapitalization(.none)
-                    .foregroundColor(.white)
-                    .accentColor(.white)
-                Divider()
-                    .background(Color.white)
-                    .padding(.bottom, 20.0)
-                // パスワード
-                Text("パスワード")
-                    .foregroundColor(.white)
-                SecureField("", text: $password)
-                    .autocapitalization(.none)
-                    .foregroundColor(.white)
-                    .accentColor(.white)
-                Divider()
-                    .background(Color.white)
-                    .padding(.bottom, 20.0)
-                // ログインボタン
-                Button {
-                    
-                } label: {
-                    Text("ログイン")
-                        .modifier(loginButton())
+        NavigationStack {
+            ZStack {
+                // 全体の背景カラー
+                backGroundColor.ignoresSafeArea()
+                
+                VStack(alignment: .leading) {
+                    // タイトル
+                    Text("Linneにログイン")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .padding(.vertical, 60.0)
+                    // メールアドレス
+                    Text("メールアドレス")
+                        .foregroundColor(.white)
+                    TextField("", text: $email)
+                        .autocapitalization(.none)
+                        .foregroundColor(.white)
+                        .accentColor(.white)
+                    Divider()
+                        .background(Color.white)
+                        .padding(.bottom, 20.0)
+                    // パスワード
+                    Text("パスワード")
+                        .foregroundColor(.white)
+                    SecureField("", text: $password)
+                        .autocapitalization(.none)
+                        .foregroundColor(.white)
+                        .accentColor(.white)
+                    Divider()
+                        .background(Color.white)
+                        .padding(.bottom, 20.0)
+                    // ログインボタン
+                    Button {
+                        // パスワード一致判定
+                        if (password == "12345") {
+                            self.loginFlag = true
+                        }
+                    } label: {
+                        Text("ログイン")
+                            .modifier(loginButton())
+                    }
+                    .navigationDestination(isPresented: $loginFlag) {
+                        HomeView()
+                        // 戻るボタンを非表示
+                            .navigationBarBackButtonHidden(true)
+                    }
+                    .padding(.bottom, 100.0)
+                    Spacer()
                 }
-                .padding(.bottom, 100.0)
-                Spacer()
+                .padding()
             }
-            .padding()
-        }
-        .onTapGesture {
-            UIApplication.shared.closeKeyboard()
+            .onTapGesture {
+                UIApplication.shared.closeKeyboard()
+            }
         }
     }
 }
