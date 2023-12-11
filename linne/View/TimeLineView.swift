e55306a4898dcb6431bafbf602dbe1996b93549f
daisuke.abe <commityneo@gmail.com>
2023-12-11 22:00:17 +0900

    投稿画面の作成

--- linne/View/TimeLineView.swift
+++ linne/View/TimeLineView.swift
@@ -7,34 +7,65 @@
 
 import SwiftUI
 
+struct Tweet: Identifiable {
+    var id = UUID()
+    var username: String
+    var tweetText: String
+}
+
 struct TimeLineView: View {
+    @State private var tweets: [Tweet] = [
+        Tweet(username: "John Doe", tweetText: "This is my first tweet!"),
+        Tweet(username: "Jane Smith", tweetText: "Excited to use SwiftUI for the first time!"),
+        Tweet(username: "TwitterUser", tweetText: "Hello SwiftUI!")
+    ]
+    
+    @State private var newTweetText: String = ""
+    
     var body: some View {
-        @State var selection = 1
-        
-        
-        
-        TabView(selection: $selection) {
-            
-            LoginView()
-                .tabItem {
-                    Label("", systemImage: "house")
+        NavigationView {
+            VStack(alignment: .leading) {
+                // Timeline
+                List(tweets) { tweet in
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text(tweet.username)
+                            .font(.headline)
+                        
+                        Text(tweet.tweetText)
+                            .font(.body)
+                            .foregroundColor(.secondary)
+                    }
+                    .padding(.vertical, 8)
                 }
-            
-            ShareView()
-                .tabItem {
-                    Label("", systemImage: "2.circle")
+                .listStyle(PlainListStyle())
+                
+                // Compose Tweet Box
+                HStack {
+                    TextField("What's happening?", text: $newTweetText)
+                        .padding()
+                        .background(Color(.secondarySystemBackground))
+                        .cornerRadius(10)
+                    
+                    Button("Tweet") {
+                        if !newTweetText.isEmpty {
+                            tweets.append(Tweet(username: "CurrentUser", tweetText: newTweetText))
+                            newTweetText = ""
+                        }
+                    }
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
                 }
-            
-            HomeView()
-                .tabItem {
-                    Label("", systemImage: "3.circle")
-                }
-            
-            HomeView()
-                .tabItem {
-                    Label("", systemImage: "userIcon")
-                }
-            
+                .padding()
+            }
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                // Add any action for a right navigation bar button
+            }) {
+                Image(systemName: "star")
+            }
+            )
         }
     }
 }
